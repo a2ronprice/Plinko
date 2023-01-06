a553b874028a7d96b8c192aa74f0202a1260c054
ap568@duke.edu <ap568@duke.edu>
2023-01-06 18:55:19 -0300

    sanitize username data

---
+++ Plinko/UI/LoadingPlay.swift
@@ -0,0 +1,21 @@
+//
+//  LoadingPlay.swift
+//  Plinko
+//
+//  Created by Aaron  Price on 1/6/23.
+//
+
+import Foundation
+import UIKit
+import FirebaseCore
+import FirebaseAuth
+
+class LoadingPlay: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
--- Plinko/ViewControllers/SignUp.swift
+++ Plinko/ViewControllers/SignUp.swift
@@ -17,6 +17,7 @@ class SignUp: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        errorLabel.numberOfLines = 0
         errorLabel.isHidden = true
         PassBox.isSecureTextEntry = true
         loadingDot.layer.cornerRadius = 4
@@ -31,77 +32,79 @@ class SignUp: UIViewController {
         let username = UserNameBox.text ?? "user"
         let email = EmailBox.text ?? "email"
         let password = PassBox.text ?? "pass"
-        if usernameIsGood(username: username) {
-        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
-            if let error = error as NSError? {
-            print(error)
-                self.errorLabel.isHidden = false
-                self.errorLabel.text = error.localizedDescription
-                self.loadingDot.isHidden = true
-          } else {
-            print("User signs up successfully")
-              let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
-              print("Username:", username)
-              changeRequest?.displayName = username
-              changeRequest?.commitChanges { error in
-                  if let error = error {
-                      print(error.localizedDescription)
-                  } else {
-                      print("Success changing display name to: ", username)
-                      user.username = username
-                      user.email = email
-                      user.totalPoints = 0
-                      user.totalPlays = 0
-                      user.allPlayTimes = []
-                      user.allPlayScores = []
-                      user.lastPlayTime = nil
-                      user.lastPlayScore = 0
-                      db.collection("users").document(user.email).setData([
-                        "username": user.username,
-                        "total-points": user.totalPoints,
-                        "total-plays": user.totalPlays,
-                        "last-play-time": user.lastPlayTime,
-                        "last-play-score": user.lastPlayScore,
-                        "all-play-times": user.allPlayTimes,
-                        "all-play-scores": user.allPlayScores
-                        
-                      ]) { err in
-                          if let err = err {
-                              print("Error writing document: \(err)")
-                          } else {
-                              print("Document successfully written!")
-                          }
-                      }
-                      
-                      self.performSegue(withIdentifier: "SignUpToMain", sender:self)
-                  }
-              }
-              
-              //create user
-              //push user to firestore
-              
-          }
+        if dataIsClean(username: username) {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let error = error as NSError? {
+                    print(error)
+                    self.errorLabel.isHidden = false
+                    self.errorLabel.text = error.localizedDescription
+                    self.loadingDot.isHidden = true
+                } else {
+                    print("User signs up successfully")
+                    let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+                    print("Username:", username)
+                    changeRequest?.displayName = username
+                    changeRequest?.commitChanges { error in
+                        if let error = error {
+                            print(error.localizedDescription)
+                        } else {
+                            print("Success changing display name to: ", username)
+                            user.username = username
+                            user.email = email
+                            user.totalPoints = 0
+                            user.totalPlays = 0
+                            user.allPlayTimes = []
+                            user.allPlayScores = []
+                            user.lastPlayTime = nil
+                            user.lastPlayScore = 0
+                            db.collection("users").document(user.email).setData([
+                                "username": user.username,
+                                "total-points": user.totalPoints,
+                                "total-plays": user.totalPlays,
+                                "last-play-time": user.lastPlayTime,
+                                "last-play-score": user.lastPlayScore,
+                                "all-play-times": user.allPlayTimes,
+                                "all-play-scores": user.allPlayScores
+                                
+                            ]) { err in
+                                if let err = err {
+                                    print("Error writing document: \(err)")
+                                } else {
+                                    print("Document successfully written!")
+                                }
+                            }
+                            
+                            self.performSegue(withIdentifier: "SignUpToMain", sender:self)
+                        }
+                    }
+                    
+                    //create user
+                    //push user to firestore
+                    
+                }
+                
+            }
+        } else {
+            errorLabel.isHidden = false
+            errorLabel.text = "Invalid Username \n Remove Special Characters"
+            loadingDot.isHidden = true
+            print("Bad Username")
             
-        }
         }
     }
     override var prefersStatusBarHidden: Bool {
         return true
     }
     
-    func usernameIsGood(username : String) -> Bool {
-        if containsEmojis(string: username) { return false}
-        return true
-    }
-    
-    func containsEmojis(string: String) -> Bool {
-        let forbiddenScalars = CharacterSet.symbols
-        for scalar in string.unicodeScalars {
-            if forbiddenScalars.contains(scalar) {
-                return true
+    func dataIsClean(username: String) -> Bool {
+        let allowedScalars = CharacterSet.alphanumerics
+        for scalar in username.unicodeScalars {
+            if allowedScalars.contains(scalar) != true {
+                return false
             }
         }
-        return false
+        return true
     }
 }
+
 
